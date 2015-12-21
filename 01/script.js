0ffbaad514291e96d718918672cfb9f7c68f7667
Vasil Vanchuk <v.vanchuk@tut.by>
2015-12-21 21:15:11 +0300

    01 - simple actions from controller

--- 01/script.js
+++ 01/script.js
@@ -1,6 +1,15 @@
 angular.module('NgBookApp', [])
   .controller('MyController', function($scope, $timeout) {
     $scope.clock = {};
+    $scope.counter = 0;
+
+    $scope.add = function(amount) {
+      $scope.counter += amount;
+    };
+    $scope.subtract = function(amount) {
+      $scope.counter -= amount;
+    };
+
     var updateClock = function() {
       $scope.clock.now = new Date();
       $timeout(function() {
