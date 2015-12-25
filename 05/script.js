2c521e45de91c61a1f45031a68e192ea1b1a413f
Vasil Vanchuk <v.vanchuk@tut.by>
2015-12-25 22:26:20 +0300

    05 - Clarify directive options

--- 05/script.js
+++ 05/script.js
@@ -1,4 +1,36 @@
 angular.module('NgBookApp', [])
+  .directive('myDirective',
+    function($timeout, UserDefinedService) {
+      // directive definition goes here
+
+      // A directive definition object
+      return {
+        //restrict: String,
+        //priority: Number,
+        //terminal: Boolean,
+        //template: String or Template Function:
+        //  function(tElement, tAttrs) {...},
+        //templateUrl: String,
+        //replace: Boolean or String,
+        //scope: Boolean or Object,
+        //transclude: Boolean,
+        //controller: String or
+        //  function(scope, element, attrs, transclude, otherInjectables) { ... },
+        //controllerAs: String,
+        //require: String,
+        //link: function(scope, iElement, iAttrs) { ... },
+        //compile: // return an Object OR the link function
+        // as in below:
+        //  function(tElement, tAttrs, transclude) {
+        //    return {
+        //      pre: function(scope, iElement, iAttrs, controller) { ... },
+        //      post: function(scope, iElement, iAttrs, controller) { ... }
+        //    }
+        //  // or
+        //  return function postLink(...) { ... }
+        //}
+      };
+    })
   .controller('MyController',
     ['$scope',
       function($scope) {
