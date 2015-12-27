f6a4c92483bb71098d2313db87b16927c05aceef
Vasil Vanchuk <v.vanchuk@tut.by>
2015-12-27 16:18:08 +0300

    07 - using services

--- 07/script.js
+++ 07/script.js
@@ -4,7 +4,28 @@ angular.module('NgBookApp', [])
       greet: function(msg) {
         alert(msg);
       }
-    }
+    };
+  })
+  .factory('githubService', function($http) {
+    var githubUrl = 'https://api.github.com';
+
+    var runUserRequest = function(username, path) {
+      // Return the promise from the $http service
+      // that calls the Github API using JSONP
+      return $http({
+        method: 'JSONP',
+        url: githubUrl + '/users/' +
+        username + '/' +
+        path + '?callback=JSON_CALLBACK'
+      });
+    };
+    // Return the service object with a single function
+    // events
+    return {
+      events: function(username) {
+        return runUserRequest(username, 'events');
+      }
+    };
   })
   .controller('MyController',
     ['$scope', 'greeter',
@@ -12,10 +33,27 @@ angular.module('NgBookApp', [])
         $scope.sayHello = function() {
           greeter.greet("Hello!");
         };
-      }]);
+      }])
+  .controller('ServiceController',
+    function($scope, githubService) {
+      // Watch for changes on the username property.
+      // If there is a change, run the function
+      $scope.$watch('username', function(newUsername) {
+        // uses the $http service to call the
+        // GitHub API and returns the resulting promise
+        githubService.events(newUsername)
+          .success(function(data, status, headers) {
+            // the success function wraps
+            // the response in data
+            // so we need to call data.data to
+            // fetch the raw data
+            $scope.events = data.data;
+          })
+      });
+    });
 
 // it works like:
- // Load the app with the injector
+// Load the app with the injector
 //  var injector = angular.injector(['ng', 'myApp']);
 // Load the $controller service with the injector
 //  var $controller = injector.get('$controller');
@@ -25,6 +63,7 @@ angular.module('NgBookApp', [])
 //  var MyController = $controller('MyController', {$scope: scope});
 //  console.log(injector.annotate(function($q, greeter) {})); // ["$q", "greeter"]
 
+/*
 var aControllerFactory =
   function aController($scope, greeter) {
     console.log("LOADED controller", greeter);
@@ -46,3 +85,4 @@ var injector = angular.injector(['ng', 'myApp']),
   newScope = rootScope.$new();
 // Invoke the controller
 controller('MyController', { $scope: newScope });
+*/
