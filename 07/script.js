60fec33254f54a000235843829cd0bf410b4ab25
Vasil Vanchuk <v.vanchuk@tut.by>
2015-12-27 16:02:32 +0300

    07 - explicit annotation

--- 07/script.js
+++ 07/script.js
@@ -24,3 +24,25 @@ angular.module('NgBookApp', [])
 // which is how angular does it at runtime
 //  var MyController = $controller('MyController', {$scope: scope});
 //  console.log(injector.annotate(function($q, greeter) {})); // ["$q", "greeter"]
+
+var aControllerFactory =
+  function aController($scope, greeter) {
+    console.log("LOADED controller", greeter);
+    // ... Controller
+  };
+aControllerFactory.$inject = ['$scope', 'greeter'];
+// Greeter service
+var greeterService = function() {
+  console.log("greeter service");
+};
+// Our app controller
+angular.module('myApp', [])
+  .controller('MyController', aControllerFactory)
+  .factory('greeter', greeterService);
+// Grab the injector and create a new scope
+var injector = angular.injector(['ng', 'myApp']),
+  controller = injector.get('$controller'),
+  rootScope = injector.get('$rootScope'),
+  newScope = rootScope.$new();
+// Invoke the controller
+controller('MyController', { $scope: newScope });
